acf1987be605ce66ca4d186b0dd4194ca940c35f
Scott <robertscottharrington@gmail.com>
2022-03-01 16:39:26 -0500

    updated project to use twilio instead of SMTP

--- mailer.py
+++ mailer.py
@@ -5,7 +5,7 @@ class Mailer:
 
     def __init__(self, receiver):
         self.sid = "ACdcbccef757b3123bbaa9ed88b9aae82e"
-        self.token = "576f8c3cd4c0dfba6e8644c118547293"
+        self.token = "5f14328a45c61a4d623b838e045d8421"
         self.receiver = receiver
         self.sender = "+19206268558"
 
