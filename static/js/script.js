c9a2dce0b43d8a8ead0382ec2d89612cb0d4bc69
Scott <robertscottharrington@gmail.com>
2022-07-11 14:14:36 -0400

    added sticky nav, adjusted fade in on some sections

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,7 @@
 const smallSections = document.querySelectorAll('.section--small')
 const largeSections = document.querySelectorAll('.section--large')
+const nav = document.querySelector('.navbar')
+const title = document.querySelector('#title')
 const obsOptSmall = {
 root: null,
 threshold: 0.25
@@ -27,3 +29,23 @@ const obsLarge = new IntersectionObserver(showSection, obsOptLarge)
 largeSections.forEach(value => {
     obsLarge.observe(value)
 })
+
+function stickyNav(entries, observer) {
+    if(!entries[0].isIntersecting) {
+    nav.classList.add('sticky')
+    } else {
+    nav.classList.remove('sticky')
+    }
+
+
+}
+
+const navObsOpts = {
+    root: null,
+    threshold: 0.1,
+    rootMargin: `-${nav.getBoundingClientRect().height}px`
+
+}
+
+const navObs = new IntersectionObserver(stickyNav, navObsOpts)
+navObs.observe(title)
